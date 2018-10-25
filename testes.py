787cf465236ea10603f0ce694b392999e050429d
Unknown <gabrieldfm13@gmail.com>
2018-10-25 00:23:59 -0300

    Criação novas funções

--- testes.py
+++ testes.py
@@ -1,2 +1,12 @@
 def cria_conta(numero, titular):
     conta = {"numero": numero, "titula": titular}
+    return conta
+
+def deposita(conta, valor):
+    conta["saldo"] += valor
+
+def saque(conta, valor):
+    conta["saldo"] -= valor
+
+def extrato(conta):
+    print("Saldo é {}".format(conta["saldo"]))
